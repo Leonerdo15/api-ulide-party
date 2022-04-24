4df3a0cd5451d5752c498bb5e48395984f8d32c6
Leonerdo15 <leonardolage10@gmail.com>
2022-04-24 22:51:15 +0100

    yellow circle.js

--- public/circle.js
+++ public/circle.js
@@ -40,7 +40,7 @@ async function initMap() {
             strokeColor: "#ffb500",
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: "#FF0000",
+            fillColor: "#ffb500",
             fillOpacity: 0.35,
             map,
             center: { lat: json[i].st_x, lng: json[i].st_y },
