03e45105f9a46584a7b7eb87e8adae34452618a8
Leonerdo15 <leonardolage10@gmail.com>
2022-04-24 22:49:33 +0100

    yellow circle.js

--- public/circle.js
+++ public/circle.js
@@ -37,7 +37,7 @@ async function initMap() {
 
     for (let i = 0; i < json.length; i++) {
         const cityCircle = new google.maps.Circle({
-            strokeColor: "#FF0000",
+            strokeColor: "#ffb500",
             strokeOpacity: 0.8,
             strokeWeight: 2,
             fillColor: "#FF0000",
