396058d98f43dcd4628184f457f669250811fff9
Leonerdo15 <leonardolage10@gmail.com>
2022-04-25 15:45:01 +0100

    organize

--- routes/spot.js
+++ routes/spot.js
@@ -36,15 +36,11 @@ const getSpotsArea = (request, response) => {
 const getSpotById = (request, response) => {
     const id = parseInt(request.params.id)
 
-    client.query('SELECT * FROM spots WHERE sp_id = $1', [id], (error, results) => {
+    client.query('SELECT * FROM spots WHERE sp_id = $1;' +
+        'UPDATE spots SET sp_views = sp_views + 1 WHERE sp_id = $1;', [id], (error, results) => {
         if (error) {
             throw error
-        } client.query('UPDATE spots SET sp_views = sp_views + 1 WHERE sp_id = $1',[id], (error, results) =>{
-            if(error){
-                throw error
-            }
-            response.status(200).json(results.rows)
-        })
+        }
 
         response.status(200).json(results.rows)
     })
