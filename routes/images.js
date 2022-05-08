892142f05c72cdf2ce3abdd2f14cd5cd1c8f711f
Leonerdo15 <leonardolage10@gmail.com>
2022-05-08 15:36:27 +0100

    new folder

--- routes/images.js
+++ routes/images.js
@@ -9,37 +9,24 @@ const cloudinary = require('cloudinary').v2
 
 
 router.post('/save',  function (req,res) {
-    let name = req.params.name
+
+    cloudinary.config({
+        cloud_name: 'ulide-party',
+        api_key: '757193529144895',
+        api_secret: 'zWOHPeQOb7-ZiQBXHG5STwqITJk'
+    });
+
     let form = new formidable.IncomingForm();
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
     form.parse(req, function (err, fields, files) {
+
         const oldpath = files.filetoupload.filepath;
-        console.log(oldpath)
-
-
-        // const newpath = newUrl + files.filetoupload.originalFilename;
-        // console.log(newpath)
-        // fs.rename(oldpath, newpath, function (err) {
-        //     console.log("ccccccccccccccccccccccc")
-        //     if (err) throw err;
-        //     res.write('File uploaded and moved!');
-        //     res.end();
-        // });
-        cloudinary.config({
-            cloud_name: 'ulide-party',
-            api_key: '757193529144895',
-            api_secret: 'zWOHPeQOb7-ZiQBXHG5STwqITJk'
-        });
-
-        // console.log(cloudinary.url(oldpath))
-
         let imageName = files.filetoupload.originalFilename
         let position = imageName.indexOf(".")
         let newImageName = imageName.substring(0, position)
 
         cloudinary.uploader.upload(oldpath,
             {
-               public_id: newImageName
+               public_id: "images/" + newImageName
             },
             function (err, callResult) {
                 console.log(err, callResult)
@@ -47,13 +34,6 @@ router.post('/save',  function (req,res) {
         res.write(cloudinary.url(newImageName))
         res.end()
     });
-})
-
-router.get('/teste', async function (req, res, next) {
-    const x = {
-      bata:"teste"
-    }
-    res.status(200).send(x)
 })
 
 
