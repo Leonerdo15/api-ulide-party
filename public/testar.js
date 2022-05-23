4e4c592137e9f5f33350845d7455f27fa97e2a53
Leonerdo15 <leonardolage10@gmail.com>
2022-05-23 21:30:08 +0100

    testar

---
+++ public/testar.js
@@ -0,0 +1,21 @@
+$(document).ready(function (e) {
+    $("#form").on('submit', (function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://ulide-party-api.herokuapp.com//api/image/save",
+            type: "POST",
+            data: new FormData(this),
+            dataType: 'json',
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }));
+});
