9fecfff0d8bc1e0c192d2148af8ee41a128e05a6
Leonerdo15 <leonardolage10@gmail.com>
2022-05-23 22:12:07 +0100

    testar

--- routes/images.js
+++ routes/images.js
@@ -19,7 +19,8 @@ router.post('/save',  function (req,res) {
 
         const newpath = newUrl + files.fileupload.originalFilename;
         console.log(newpath)
-        fs.rename(oldpath, newpath, function () {
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
             res.write('File uploaded and moved!');
             res.end();
         });
