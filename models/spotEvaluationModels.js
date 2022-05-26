2a093d3afb72f6a713abad6151f5e344f73054ab
Leonerdo15 <leonardolage10@gmail.com>
2022-05-26 14:08:40 +0100

    inner join user on spotEvaluation

--- models/spotEvaluationModels.js
+++ models/spotEvaluationModels.js
@@ -2,7 +2,7 @@ const pool = require('../database/connection');
 
 module.exports.getSpotEvaluations = async function (id) {
     try {
-        let sql =  `select * from spot_evaluations, TO_CHAR(se_date :: DATE, ' dd, Mon, yyyy') where se_sp_id = ${id}`
+        let sql =  `select se_rate, se_comment, TO_CHAR(se_date :: DATE, ' dd, Mon, yyyy'), us_name from spot_evaluations inner join users u on spot_evaluations.se_us_id = u.us_id where se_sp_id = 1`
         let result = await pool.query(sql)
         let evaluations = result.rows
         console.log(evaluations)
