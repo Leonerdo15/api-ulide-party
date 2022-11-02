6206a4c8dabfa6563a83203ad9c0ceaabd6c8e9b
Leonerdo15 <leonardolage10@gmail.com>
2022-11-02 22:39:18 +0000

    tests

--- teste/test.js
+++ teste/test.js
@@ -1,8 +1,23 @@
 const spotsModel = require('../models/spotsModels')
 
-async function teste(){
-    let teste = await spotsModel.getSpotsForListByIdOrderBy(1, 'rateDesc')
-    console.log(teste.data)
+let obj = {
+    ow_us_id: 1,
+    ow_sp_id: 1,
+    sr_text_request: "test",
+    sr_validation: false,
+    psr_ph_id: 1,
+    psr_sr_id: 1
 }
 
-teste()
+let obj2 = {
+    teste: 1,
+    teste2: 2,
+    teste3: 3
+}
+
+let obj3 = {
+    ...obj,
+    ...obj2
+}
+
+console.log(obj3)
